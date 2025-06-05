09c092dacba76c1d27c9a5315c44fdec934d0863
punyaripki <rifqithanthawi@gmail.com>
2025-06-05 08:22:08 +0700

    5/6/2025

---
+++ db_clusterbencana.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2025 at 01:41 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_clusterbencana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table db_clusterbencana.migrations: #1932 - Table &#039;db_clusterbencana.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+-- Error reading structure for table db_clusterbencana.personal_access_tokens: #1932 - Table &#039;db_clusterbencana.personal_access_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.personal_access_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`personal_access_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_clustering`
+--
+-- Error reading structure for table db_clusterbencana.tb_clustering: #1932 - Table &#039;db_clusterbencana.tb_clustering&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_clustering: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_clustering`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_databencana`
+--
+-- Error reading structure for table db_clusterbencana.tb_databencana: #1932 - Table &#039;db_clusterbencana.tb_databencana&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_databencana: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_databencana`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_datagempa`
+--
+-- Error reading structure for table db_clusterbencana.tb_datagempa: #1932 - Table &#039;db_clusterbencana.tb_datagempa&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_datagempa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_datagempa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jenisbencana`
+--
+-- Error reading structure for table db_clusterbencana.tb_jenisbencana: #1932 - Table &#039;db_clusterbencana.tb_jenisbencana&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_jenisbencana: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_jenisbencana`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kecamatan`
+--
+-- Error reading structure for table db_clusterbencana.tb_kecamatan: #1932 - Table &#039;db_clusterbencana.tb_kecamatan&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_kecamatan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_kecamatan`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kotakab`
+--
+-- Error reading structure for table db_clusterbencana.tb_kotakab: #1932 - Table &#039;db_clusterbencana.tb_kotakab&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_kotakab: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_kotakab`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kriteria`
+--
+-- Error reading structure for table db_clusterbencana.tb_kriteria: #1932 - Table &#039;db_clusterbencana.tb_kriteria&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_kriteria: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_kriteria`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_log_iterasi`
+--
+-- Error reading structure for table db_clusterbencana.tb_log_iterasi: #1932 - Table &#039;db_clusterbencana.tb_log_iterasi&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_log_iterasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_log_iterasi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_silhouette_score`
+--
+-- Error reading structure for table db_clusterbencana.tb_silhouette_score: #1932 - Table &#039;db_clusterbencana.tb_silhouette_score&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_silhouette_score: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_silhouette_score`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+-- Error reading structure for table db_clusterbencana.tb_user: #1932 - Table &#039;db_clusterbencana.tb_user&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_clusterbencana.tb_user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_clusterbencana`.`tb_user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
